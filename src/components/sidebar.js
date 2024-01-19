8b9c372f39f0e29662b49561e106615985b1556d
nedjwab <nedjwa.bouraiou@univ-constatine2.dz>
2024-01-19 13:14:00 +0100

    Add sidebar hover effect

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,27 +4,27 @@ import '../App.css'
 
 const Sidebar = () => (
   <div>
-    <aside className="mt-96 ml-9 hidden xl:block fixed" aria-label="Sidebar">
+    <aside className="mt-96 ml-9 hidden xl:block fixed" aria-label="Sidebar" data-aos="zoom-in">
       <div className="line mt-5 ml-5 mb-9" />
       <div className="rounded text-2xl">
         <ul className="text-para space-y-2 flex flex-row xl:block">
-          <li>
-            <a href="https://www.linkedin.com/in/nedjwa-bouraiou/" className=" flex items-center p-2 text-2xl font-normal text-gray-900 rounded-lg dark:text-white mb-1 ">
-              <BsLinkedin />
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/ned_jwa" className="flex  p-2 text-2xl font-normal text-gray-900 rounded-lg dark:text-white">
-              <BsTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/nedjwab" className="flex items-center p-2 text-2xl font-normal text-gray-900 rounded-lg dark:text-white">
-              <BsGithub />
-            </a>
-          </li>
-        </ul>
-      </div>
+        <li>
+        <a href="https://www.linkedin.com/in/nedjwa-bouraiou/" className="flex items-center p-2 text-2xl font-normal text-gray-900 rounded-lg dark:text-white mb-1 sidebar-icon">
+          <BsLinkedin />
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/ned_jwa" className="flex p-2 text-2xl font-normal text-gray-900 rounded-lg dark:text-white sidebar-icon">
+          <BsTwitter />
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/nedjwab" className="flex items-center p-2 text-2xl font-normal text-gray-900 rounded-lg dark:text-white sidebar-icon">
+          <BsGithub />
+        </a>
+      </li>
+              </ul>
+            </div>
     </aside>
   </div>
 );
