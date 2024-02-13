4efb1f72b2abd6af5c254b37de38d3bff14e8a15
nedjwab <nedjwa.bouraiou@univ-constatine2.dz>
2024-02-13 19:57:32 +0100

    Fix analytics

--- src/App.js
+++ src/App.js
@@ -8,15 +8,25 @@ import Contact from './components/contact/Contact';
 import MouseParticles from 'react-mouse-particles';
 import Navbar from './components/navbar/Navbar';
 import ReactGA from 'react-ga';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+  const location = useLocation();
 
-  ReactGA.initialize('G-NSBRC3JN2S');
-  ReactGA.pageview(window.location.pathname + window.location.search);
+  useEffect(() => {
+    ReactGA.initialize('G-NSBRC3JN2S');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+
   return (
     <div className="App">
       <>
-      <MouseParticles g={1} color="random" cull="col,image-wrapper"/>
+        <MouseParticles g={1} color="random" cull="col,image-wrapper"/>
         <ParticlesBackground />
         <Sidebar />
         <Routes>
@@ -24,7 +34,7 @@ const App = () => {
             path="/"
             element={
               <>
-              <Navbar />
+                <Navbar />
                 <Home />
                 <About />
                 <Projects />
